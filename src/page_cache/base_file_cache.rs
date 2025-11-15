2d44b08e2d29437a76a2a249b51adf38edfef5b7
Kristian <kristian1599@gmail.com>
2025-11-15 18:58:47 +0000

    j

--- src/page_cache/base_file_cache.rs
+++ src/page_cache/base_file_cache.rs
@@ -18,3 +18,10 @@ pub(crate) struct BaseFileCache {
 	read_cache: RwLock<HashMap<PageID,Arc<[u8]>>>,
 
 }
+
+//NOTE: A PageRef is shared in read cache - if we take a page into write cache and remove from read cache there may still be active
+// transaction references on that page
+// when we want to write to the page, we try to get_mut() and if we fail we must rebuild the page
+// when readers try to read a page that isn't in the read cache it must load from disk
+// as soon as committed pages are flushed the route page is updated to point to the newer pages and
+// subsequent readers follow the new chain and will load those pages from disk skipping older versions
